1c5cf5ca2b6837b2fdbef96d05e16e44a85a3aa4
ThejeshVenkata <thejesh219@gmail.com>
2016-09-24 15:44:31 +0530

    Added textRank feature

--- FeatureExtraction/FeatureExtractor.py
+++ FeatureExtraction/FeatureExtractor.py
@@ -1,10 +1,12 @@
 from PreProcess import doPreProcessing
 from VectorSpaceModel import convertToVSM
 from TF_ISF import calcMeanTF_ISF
+from summa import summarizer
+
 
 # Feature Vector -
-# Features - [ Mean_TF_ISF, Sentence Length,  Sentence Position    ....           ]
-# Indexes  - [     0      ,        1       ,          2        ,   ....           ]
+# Features - [ Mean_TF_ISF, Sentence Length,  Sentence Position,   textRank,   ....         ]
+# Indexes  - [     0      ,        1       ,          2        ,   3       ,   ....         ]
 
 def extractFeatures(ip_doc):
 	ip_file = open(ip_doc)
@@ -14,6 +16,7 @@ def extractFeatures(ip_doc):
 	sentences_len = len(sentences)
 	maxlen = max(lengths)
 	VSM = convertToVSM(sentences)
+	sentenceRanks=summarizer.summarize(ip_doc,1,None,"english",False,True)
 	featureVectors = []
 	for i in range(sentences_len):
 		fVect = []
@@ -23,5 +26,6 @@ def extractFeatures(ip_doc):
 			fVect.append(0.9 - 1.6*i/sentences_len)
 		else:
 			fVect.append(0.1 + 1.6*(min([i-sentences_len/2, sentences_len/2]))/sentences_len)
-		featureVectors.append(fVect)	
+		fVect.append(sentenceRanks[i][1])
+		featureVectors.append(fVect)
 	print featureVectors
