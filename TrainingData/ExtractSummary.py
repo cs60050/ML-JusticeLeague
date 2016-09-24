372137fbe921b4d4ae4e84b1d265feabbc2ff5a5
Supradeep Allu <supradeep95@gmail.com>
2016-09-24 21:31:24 +0530

    Added dataset description. Bugfix to avoid improper sent_tokenization like in Doc #116.

--- TrainingData/ExtractSummary.py
+++ TrainingData/ExtractSummary.py
@@ -1,31 +1,42 @@
+from os import listdir
+from random import shuffle
 from PreProcess import doPreProcessing
-import os
 
-count = 1
-for filename in os.listdir("./training/docs"):
-	try:
-		ip_file = open("./training/docs/"+filename,"r")
-		ip_doc = ip_file.read()
-		ip_file.close()
-		smallsumm, largesumm, fulldoc = doPreProcessing(ip_doc)
-		f = open("./FullDocs/"+str(count)+"_fulldoc.txt","w")
-		for sent in fulldoc:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		f = open("./SmallSumms/"+str(count)+"_smallsumm.txt","w")
-		for sent in smallsumm:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		f = open("./LargeSumms/"+str(count)+"_largesumm.txt","w")
-		for sent in largesumm:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		count+=1
-	except:
-		continue
+def extractSummary():
+    count = 1
+    op_folder = "./Training/"
+
+    data_dir = listdir("./Dataset/training/docs")
+    shuffle(data_dir)
+    for filename in data_dir:
+
+        if count == 121:
+            op_folder = "./Test/"
+
+        ip_file = open("./Dataset/training/docs/"+filename, "r")
+
+        ip_doc = ip_file.read()
+        ip_file.close()
+        smallsumm, largesumm, fulldoc = doPreProcessing(ip_doc)
+        op_file = open(op_folder+"FullDocs/"+str(count)+"_fulldoc.txt", "w")
+        for sent in fulldoc:
+            for word in sent:
+                op_file.write(word+" ")
+            op_file.write(".\n")
+        op_file.close()
+        op_file = open(op_folder+"SmallSumms/"+str(count)+"_smallsumm.txt", "w")
+        for sent in smallsumm:
+            for word in sent:
+                op_file.write(word+" ")
+            op_file.write(".\n")
+        op_file.close()
+        op_file = open(op_folder+"LargeSumms/"+str(count)+"_largesumm.txt", "w")
+        for sent in largesumm:
+            for word in sent:
+                op_file.write(word+" ")
+            op_file.write(".\n")
+        op_file.close()
+        count += 1
+
+if __name__ == "__main__":
+    extractSummary()
