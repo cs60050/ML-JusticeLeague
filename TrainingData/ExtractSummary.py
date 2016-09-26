ea2cd4ddf598de6f154ca12171fc0e1651f6b3f5
ThejeshVenkata <thejesh219@gmail.com>
2016-09-27 00:24:33 +0530

    revert

--- TrainingData/ExtractSummary.py
+++ TrainingData/ExtractSummary.py
@@ -2,38 +2,6 @@ from os import listdir
 from random import shuffle
 from PreProcess import doPreProcessing
 
-<<<<<<< HEAD
-count = 1
-for filename in os.listdir("./training/docs"):
-	try:
-		ip_file = open("./training/docs/"+filename,"r")
-		ip_doc = ip_file.read()
-		ip_file.close()
-		smallsumm, largesumm, fulldoc = doPreProcessing(ip_doc)
-		f = open("./FullDocs/"+str(count)+"_fulldoc.txt","w")
-		for sent in fulldoc:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		f = open("./SmallSumms/"+str(count)+"_smallsumm.txt","w")
-		for sent in smallsumm:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		f = open("./LargeSumms/"+str(count)+"_largesumm.txt","w")
-		for sent in largesumm:
-			for word in sent:
-				f.write(word+" ")
-			f.write(".\n")
-		f.close()
-		count+=1
-	except:
-<<<<<<< HEAD
-<<<<<<< HEAD
-		continue
-=======
 def extractSummary():
     count = 1
     op_folder = "./Training/"
@@ -72,10 +40,3 @@ def extractSummary():
 
 if __name__ == "__main__":
     extractSummary()
->>>>>>> cs60050/master
-=======
-		continue
->>>>>>> parent of 5c870b7... sentences without stemming are also required for training
-=======
-		continue
->>>>>>> parent of 5c870b7... sentences without stemming are also required for training
--- TrainingData/PreProcess.py
+++ TrainingData/PreProcess.py
@@ -3,32 +3,6 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
 
 def doPreProcessing(ip_doc):
-<<<<<<< HEAD
-	ps = PorterStemmer()
-	fulldoc = []
-	smallsumm = []
-	largesumm = []
-	sentences = sent_tokenize(ip_doc)
-	for sent in sentences:
-		tokens =  word_tokenize(sent)
-		words = [w.lower() for w in tokens]
-		words = list(filter(lambda x: "'" not in x and\
-									  "`" not in x and\
-									  "." not in x and\
-									  "," not in x and\
-									  "-" not in x\
-									  , words))	
-		words = [str(ps.stem(word)) for word in words if word not in stopwords.words('english')]	
-		fulldoc.append(words[1:])
-		if(words[0]=="1"):
-			smallsumm.append(words[1:])
-			largesumm.append(words[1:])
-		elif(words[0]=="2"):
-			largesumm.append(words[1:])
-<<<<<<< HEAD
-<<<<<<< HEAD
-	return smallsumm,largesumm,fulldoc,docwithoutstemming
-=======
     ps = PorterStemmer()
     fulldoc = []
     smallsumm = []
@@ -52,10 +26,3 @@ def doPreProcessing(ip_doc):
             elif(words[0]=="2"):
                 largesumm.append(words[1:])
     return smallsumm,largesumm,fulldoc
->>>>>>> cs60050/master
-=======
-	return smallsumm,largesumm,fulldoc
->>>>>>> parent of 5c870b7... sentences without stemming are also required for training
-=======
-	return smallsumm,largesumm,fulldoc
->>>>>>> parent of 5c870b7... sentences without stemming are also required for training
