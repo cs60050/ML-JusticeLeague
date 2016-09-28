33f2bd71fe840adfe8106fbbbd9e0bdf7d8e1939
Supradeep Allu <supradeep95@gmail.com>
2016-09-28 16:26:21 +0530

    Replaced Porter with Snowball Fix to avoid losing tokens containing the symbols being filtered

--- TrainingData/PreProcess.py
+++ TrainingData/PreProcess.py
@@ -1,28 +1,27 @@
 from nltk.corpus   import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.stem import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
 
 def doPreProcessing(ip_doc,count):
-    ps = PorterStemmer()
+    ss = SnowballStemmer("english")
     fulldocwithoutStemming = []
     fulldoc = []
     smallsumm = []
     largesumm = []
     sentences = ip_doc.split('\n')
-    print count
     for sent in sentences:
         tokens =  word_tokenize(sent)
         words = tokens
-        words = list(filter(lambda x: "'" not in x and\
-                                      "`" not in x and\
-                                      "." not in x and\
-                                      "," not in x and\
-                                      "-" not in x\
+        words = list(filter(lambda x: x != "'" and\
+                                      x != "`" and\
+                                      x != "." and\
+                                      x != "," and\
+                                      x != "-"\
                                       , words))
-        if words:
+        if tokens:
             fulldocwithoutStemming.append([str(word) for word in words][1:])
-        words = [w.lower() for w in tokens]
-        words = [str(ps.stem(word)) for word in words if word not in stopwords.words('english')]
+        words = [w.lower() for w in words]
+        words = [str(ss.stem(word)) for word in words if word not in stopwords.words('english')]
         if words:
             fulldoc.append(words[1:])
             if(words[0]=="1"):
