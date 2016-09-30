d72be218513af6e61563aa7eb9312f592aef15d2
doer189 <akhil.konda135@gmail.com>
2016-09-30 18:08:06 +0530

    sentence tokenization changed

--- FeatureExtraction/PreProcess.py
+++ FeatureExtraction/PreProcess.py
@@ -1,6 +1,6 @@
 from nltk.corpus   import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.stem import PorterStemmer
+from nltk.stem import SnowballStemmer
 import nltk.data
 import sys
 
@@ -10,7 +10,7 @@ sys.setdefaultencoding('utf8')
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') 
 
 def doPreProcessing(ip_doc,ip_docWithoutStemming):
-	ps = PorterStemmer()
+	ss = SnowballStemmer("english")
 	fulldoc = []
 	lengths = []
 	sentencesList = []
@@ -34,7 +34,7 @@ def doPreProcessing(ip_doc,ip_docWithoutStemming):
 										  ";" not in x\
 										  , words))
 									  
-			words = [str(ps.stem(word)) for word in words if word not in stopwords.words('english')]	
+			words = [str(ss.stem(word)) for word in words if word not in stopwords.words('english')]	
 			length = len(words)	
 
 			fulldoc.append(words)
