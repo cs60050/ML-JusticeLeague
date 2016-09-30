b547a53e17da1da49284cfac5b081b76c887d7a9
Sai Sriharsha Annepu <annepuharsha@gmail.com>
2016-09-30 18:27:53 +0530

    Wordnet Feature Added

--- FeatureExtraction/FeatureExtractor.py
+++ FeatureExtraction/FeatureExtractor.py
@@ -3,13 +3,15 @@ from PreProcess import doPreProcessing
 from VectorSpaceModel import convertToVSM
 from TF_ISF import calcMeanTF_ISF
 from tRank import modifiedSummarizer
+from wordNet import wordNetFeature
 from nltk import pos_tag
 
 # Feature Vector -
-# Features - [ Mean_TF_ISF, Sentence Length,  Sentence Position,   textRank,   ProperNouns ,    Numerical Data        ]
-# Indexes  - [     0      ,        1       ,          2        ,   3       ,   4           ,     5                    ]
+# Features - [ Mean_TF_ISF, Sentence Length,  Sentence Position,   textRank,   ProperNouns ,    Numerical Data,   Wordnet        ]
+# Indexes  - [     0      ,        1       ,          2        ,   3       ,   4           ,     5            ,      6        ]
 
 def extractFeatures(ip_doc,ip_docWithoutStemming):
+    file_name = ip_doc
     ip_file = open(ip_doc)
     ip_doc = ip_file.read()
     ip_file.close()
@@ -25,6 +27,7 @@ def extractFeatures(ip_doc,ip_docWithoutStemming):
     VSM_deepCopy=VSM[:]
     summarizer = modifiedSummarizer()
     sentenceRanks=summarizer.summarize(VSM_deepCopy,1,None,"english",False,True)
+    wordNetWeights = wordNetFeature(file_name)
     featureVectors = []
     for i in range(sentences_len):
         if lengths[i] != 0:     
@@ -56,8 +59,12 @@ def extractFeatures(ip_doc,ip_docWithoutStemming):
             nnd = len([nd for nd in sentences[i] if nd.translate(None, '.,%').isdigit()])
             fVect.append(nnd/lengths[i])
 
-
+            # Wordnet
+            fVect.append(wordNetWeights[i])
+            
+            
             featureVectors.append(fVect)
+            
 
 
     print featureVectors
